9fac4bf7c7df0fe98969ab0f33127d2d0573adf4
Zach Zaiman <zzaiman@emory.edu>
2023-05-26 14:30:57 -0400

    updates

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ mentors = pd.read_csv("data/mentors.csv")
 partners = pd.read_csv("data/partners.csv")
 sch1 = pd.read_csv("data/schedule_day1.csv")
 sch2 = pd.read_csv("data/schedule_day2.csv")
+sch3 = pd.read_csv("data/schedule_day3.csv")
+sch4 = pd.read_csv("data/schedule_day4.csv")
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,7 +19,10 @@ def index():
                            n_mentors=len(mentors), mentors=mentors,
                            n_partners=len(partners), partners=partners,
                            n_s1=len(sch1), sch1=sch1,
-                           n_s2=len(sch2), sch2=sch2)
+                           n_s2=len(sch2), sch2=sch2,
+                           n_s3=len(sch3), sch3=sch3,
+                           n_s4=len(sch4), sch4=sch4,
+                           )
 
 if __name__ == '__main__':
     app.run(use_reloader = True, debug=True)
