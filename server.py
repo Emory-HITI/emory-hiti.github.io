22f851c51d32b000335a55cf93d584efce5fbec9
Zach Zaiman <zzaiman@emory.edu>
2023-06-02 10:56:29 -0400

    updated website

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 mentors = pd.read_csv("data/mentors.csv")
 partners = pd.read_csv("data/partners.csv")
+organizers = pd.read_csv("data/organizers.csv")
 sch1 = pd.read_csv("data/schedule_day1.csv")
 sch2 = pd.read_csv("data/schedule_day2.csv")
 sch3 = pd.read_csv("data/schedule_day3.csv")
@@ -18,6 +19,7 @@ def index():
     return render_template('index.html',
                            n_mentors=len(mentors), mentors=mentors,
                            n_partners=len(partners), partners=partners,
+                           n_organizers=len(organizers), organizers=organizers,
                            n_s1=len(sch1), sch1=sch1,
                            n_s2=len(sch2), sch2=sch2,
                            n_s3=len(sch3), sch3=sch3,
